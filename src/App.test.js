cd1c717eb6249d9739151bb7eae89789269548b4
Walid Benjehd <walidbenjehd@gmail.com>
2017-08-23 18:51:02 +0100

    integrate Unit test separate logic from component to service and improve datepicker ui

--- src/App.test.js
+++ src/App.test.js
@@ -10,24 +10,22 @@ import Myservice from './form/service';
 import MyForm from './form/form';
 
 
-describe("test service", () => {
-      it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<App />, div);
-    });
-      //test Myservice CalculateDiff function
-      it('return should retrun 5', ()=> {
-        expect(Myservice.calculateDiff(moment().subtract(5, 'days'),moment().subtract(10, 'days'))).to.eql(5);
-      })
-      it('should fail', ()=> {
-        expect(Myservice.calculateDiff(moment().subtract(1, 'days'),moment().subtract(10, 'days'))).to.eql(5);
-      })
-      //negative value should return positive one 
-      it('it should return a positive 6', ()=> {
-        expect(Myservice.calculateDiff(moment().subtract(8, 'days'),moment().subtract(2, 'days'))).to.eql(6);
-      })
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+//test Myservice CalculateDiff function
+it('return should retrun 5', ()=> {
+  expect(Myservice.calculateDiff(moment().subtract(5, 'days'),moment().subtract(10, 'days'))).to.eql(5);
 })
-//test MyForm component if contain Label
+it('should fail', ()=> {
+  expect(Myservice.calculateDiff(moment().subtract(1, 'days'),moment().subtract(10, 'days'))).to.eql(5);
+})
+//negative value should return positive one 
+it('it should return a positive 6', ()=> {
+  expect(Myservice.calculateDiff(moment().subtract(8, 'days'),moment().subtract(2, 'days'))).to.eql(6);
+})
+
 it('it should containe date picker', ()=> {
   const wrapper = shallow(<MyForm />);
   expect(wrapper.contains(<label>Start Date  : </label>)).to.equal(true);
